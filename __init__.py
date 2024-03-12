2e83f1e76fce8da3ad178ae2033526ebbdb8d28c
ImpactFrames <jvoxel@gmail.com>
2024-03-12 12:26:32 +0000

    Update __init__.py
    lols

--- __init__.py
+++ __init__.py
@@ -1,15 +1,14 @@
+import os
 from .IFPromptMkrNode import IFPromptMkrNode
 from .IFImagePromptNode import IFImagePromptNode
 from .IFSaveTextNode import IFSaveTextNode
 from .IFDisplayTextNode import IFDisplayTextNode
-
 
 NODE_CLASS_MAPPINGS = {
     "IFPromptMkrNode": IFPromptMkrNode,
     "IFImagePromptNode": IFImagePromptNode,
     "IFSaveTextNode": IFSaveTextNode,
     "IFDisplayTextNode": IFDisplayTextNode
-    
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -19,4 +18,8 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "IFDisplayTextNode": "IF Display Text📟"
 }
 
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+EXTENSION_WEB_DIRS = {
+    "ComfyUI-IF_AI_tools": os.path.join(os.path.dirname(__file__), "js")
+}
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "EXTENSION_WEB_DIRS"]
