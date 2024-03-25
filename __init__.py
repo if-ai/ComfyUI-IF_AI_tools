4db242f095798ea61e13c987534fe61bd905bcca
ImpactFrames <jvoxel@gmail.com>
2024-03-25 00:40:36 +0000

    Update __init__.py
    display text node and js files

--- __init__.py
+++ __init__.py
@@ -1,25 +1,25 @@
 import os
+import importlib.util
+import glob
+import shutil
 from .IFPromptMkrNode import IFPrompt2Prompt
 from .IFImagePromptNode import IFImagePrompt
 from .IFSaveTextNode import IFSaveText
-#from .IFDisplayTextNode import IFDisplayText
+from .IFDisplayTextNode import IFDisplayText
 
 NODE_CLASS_MAPPINGS = {
     "IF_PromptMkr": IFPrompt2Prompt,
     "IF_ImagePrompt": IFImagePrompt,
     "IF_SaveText": IFSaveText,
-    #"IF_DisplayText": IFDisplayText
+    "IF_DisplayText": IFDisplayText
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "IF_PromptMkr": "IF Prompt to Prompt💬",
     "IF_ImagePrompt": "IF Image to Prompt🖼️",
-    "IF_SaveText": "IF Save Text📝"
-    #"IF_DisplayText": "IF Display Text📟"
+    "IF_SaveText": "IF Save Text📝",
+    "IF_DisplayText": "IF Display Text📟"
 }
 
-EXTENSION_WEB_DIRS = {
-    "ComfyUI-IF_AI_tools": os.path.join(os.path.dirname(__file__), "js")
-}
-
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "EXTENSION_WEB_DIRS"]
+WEB_DIRECTORY = "./web"
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
