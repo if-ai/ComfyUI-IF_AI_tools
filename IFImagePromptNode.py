b699d7b5fe803fe5ca4bc6b1b0c5d5cd19b490a3
ImpactFrames <jvoxel@gmail.com>
2024-03-25 14:15:41 +0000

    Update IFImagePromptNode.py
    added claude haiku api  also openai but I can't test it right now due to no tokens

--- IFImagePromptNode.py
+++ IFImagePromptNode.py
@@ -9,6 +9,10 @@ import torch
 import tempfile
 from torchvision.transforms.functional import to_pil_image
 import folder_paths
+import anthropic
+import openai
+from server import PromptServer
+from aiohttp import web
 
 class IFImagePrompt:
     def __init__(self):
@@ -18,6 +22,8 @@ class IFImagePrompt:
         self.style_prompts = self.load_presets(os.path.join(self.script_dir, "stylefiles"))
         self.base_ip = "127.0.0.1"
         self.ollama_port = "11434"
+        self.anthropic_api_key = self.get_api_key("ANTHROPIC_API_KEY")
+        self.openai_api_key = self.get_api_key("OPENAI_API_KEY")
 
     def load_presets(self, dir_path):
         presets = []
@@ -28,24 +34,39 @@ class IFImagePrompt:
                     content = file.read().strip()
                     presets.append((os.path.splitext(filename)[0], content))
         return presets
+    
+    def get_api_key(self, api_key_name):
+        api_key = os.getenv(api_key_name)
+        if api_key:
+            print(f"API key found for {api_key_name}")
+            return api_key
+        print(f"Error: {api_key_name} is required")
+        return ""
 
-    def get_vision_models(self, base_ip, ollama_port):
-        api_url = f'http://{base_ip}:{ollama_port}/api/tags'
-        try:
-            response = requests.get(api_url)
-            response.raise_for_status()
-            vision_model = [model['name'] for model in response.json()['models']]
-        except Exception as e:
-            print(f"Failed to fetch models from Ollama: {e}")
-            vision_model = []
-        return vision_model
+    def get_models(self, engine, base_ip, ollama_port):
+        if engine == "ollama":
+            api_url = f'http://{base_ip}:{ollama_port}/api/tags'
+            try:
+                response = requests.get(api_url)
+                response.raise_for_status()
+                return [model['name'] for model in response.json().get('models', [])]
+            except Exception as e:
+                print(f"Failed to fetch models from Ollama: {e}")
+                return []
+        elif engine == "anthropic":
+            return ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"]
+        elif engine == "openai":
+            return ["gpt-4-vision-preview", "gpt-4-1106-vision-preview"]
+        else:
+            print(f"Unsupported engine - {engine}")
+            return []
     
     def tensor_to_image(self, tensor):
-        #ensure tensor is on CPU
+        # Ensure tensor is on CPU
         tensor = tensor.cpu()
-        #normalize tensor 0-255 and convert to byte
+        # Normalize tensor 0-255 and convert to byte
         image_np = tensor.squeeze().mul(255).clamp(0, 255).byte().numpy()
-        #create PIL image
+        # Create PIL image
         image = Image.fromarray(image_np, mode='RGB')
         return image
 
@@ -84,21 +105,13 @@ class IFImagePrompt:
                     """)
         return system_message, user_message
 
-    def describe_picture(self, image, select_vision_model, base_ip, ollama_port, image_prompt=None, embellish_prompt=None, style_prompt=None, neg_prompt=None,
-                            seed=0, temperature=0.7, top_k=40, repeat_penalty=1.1, num_ctx=2048):
+    def describe_picture(self, image, engine, selected_model, base_ip, ollama_port, image_prompt=None, embellish_prompt=None, style_prompt=None, neg_prompt=None, temperature=0.7, max_tokens=160):
         embellish_content = next((content for name, content in self.embellish_prompts if name == embellish_prompt), "")
         style_content = next((content for name, content in self.style_prompts if name == style_prompt), "")
         neg_content = next((content for name, content in self.neg_prompts if name == neg_prompt), "")
 
-        temperature = temperature or 0.7
-        seed = seed or 0
-        top_k = top_k or 40
-        repeat_penalty = repeat_penalty or 1.1
-        num_ctx = num_ctx or 2048
-
         # Check the type of the 'image' object
         if isinstance(image, torch.Tensor):
-            
             # Convert the tensor to a PIL image
             pil_image = self.tensor_to_image(image)
         elif isinstance(image, Image.Image):
@@ -107,68 +120,152 @@ class IFImagePrompt:
             pil_image = Image.open(image)
         else:
             print(f"Invalid image type: {type(image)}. Expected torch.Tensor, PIL.Image, or file path.")
-            return "Invalid image type", "Invalid image type", "No neg content"
+            return "Invalid image type"
 
         # Convert the PIL image to base64
         buffered = BytesIO()
         pil_image.save(buffered, format="PNG")
         base64_image = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
+        available_models = self.get_models(engine, base_ip, ollama_port)
+        if available_models is None or selected_model not in available_models:
+            error_message = f"Invalid model selected: {selected_model} for engine {engine}. Available models: {available_models}"
+            print(error_message)
+            raise ValueError(error_message)
+
         system_message, user_message = self._prepare_messages(image_prompt)
 
-        api_url = f'http://{base_ip}:{ollama_port}/api/generate'
-        data = {
-            "model": select_vision_model,
-            "system": system_message,
-            "prompt": user_message,
-            "stream": False,
-            "images": [base64_image],
-            "options": {
-                "seed": seed,
-                "temperature": temperature,
-                "top_k": top_k,
-                "repeat_penalty": repeat_penalty,
-                "num_ctx": num_ctx,
-            },
-        }
-        headers = {"Content-Type": "application/json"}
-
         try:
-            response = requests.post(api_url, headers=headers, json=data)
+            generated_text = self.send_request(engine, selected_model, base_ip, ollama_port, base64_image, system_message, user_message, temperature, max_tokens)
+            description = f"{embellish_content} {generated_text} {style_content}".strip()
+            return  image_prompt, description, neg_content
+        except Exception as e:
+            print(f"Exception occurred: {e}")
+            return "Exception occurred while processing image."
+        
+    def send_request(self, engine, selected_model, base_ip, ollama_port, base64_image, system_message, user_message, temperature, max_tokens):
+        if engine == "anthropic":
+            anthropic_headers = {
+                "x-api-key": self.anthropic_api_key,
+                "anthropic-version": "2023-06-01",  
+                "Content-Type": "application/json"
+            }
+
+            data = {
+                "model": selected_model,
+                "system": system_message,
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "image",
+                                "source": {
+                                    "type": "base64",
+                                    "media_type": "image/png",
+                                    "data": base64_image
+                                }
+                            },
+                            {"type": "text", "text": user_message} 
+                        ]
+                    }
+                ],
+                "temperature": temperature,
+                "max_tokens": max_tokens
+            }
+            
+            api_url = 'https://api.anthropic.com/v1/messages'
+
+            response = requests.post(api_url, headers=anthropic_headers, json=data)
+            if response.status_code == 200:
+                response_data = response.json()
+                messages = response_data.get('content', [])
+                generated_text = ''.join([msg.get('text', '') for msg in messages if msg.get('type') == 'text'])
+                return generated_text
+            else:
+                print(f"Error: Request failed with status code {response.status_code}, Response: {response.text}")
+                return "Failed to fetch response from Anthropic."
+                
+        elif engine == "openai":
+            openai_headers = {
+                "Authorization": f"Bearer {self.openai_api_key}",
+                "Content-Type": "application/json"
+            }
+            data = {
+                "model": selected_model,
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [
+                            {"type": "text", "text": user_message},
+                            {"type": "image", "source": {"type": "base64", "media_type": "image/png", "data": base64_image}}
+                        ]
+                    }
+                ],
+            }
+            api_url = 'https://api.openai.com/v1/chat/completions'
+            response = requests.post(api_url, headers=openai_headers, json=data)
+            if response.status_code == 200:
+                response_data = response.json()
+                print("Debug Response:", response_data)
+                choices = response_data.get('choices', [])
+                if choices:
+                    choice = choices[0]
+                    messages = choice.get('message', {'content': ''})
+                    generated_text = messages.get('content', '')  
+                    return generated_text
+                else:
+                    print("No valid choices in the response.")
+                    print("Full response:", response.text)
+                    return "No valid response generated for the image."
+            else:
+                print(f"Failed to fetch response, status code: {response.status_code}")
+                print("Full response:", response.text)
+                return "Failed to fetch response from OpenAI."
+        else:
+            api_url = f'http://{base_ip}:{ollama_port}/api/generate'
+            data = {
+                "model": selected_model,
+                "system": system_message,
+                "prompt": user_message,
+                "stream": False,
+                "images": [base64_image],
+                "options": {
+                    "temperature": temperature
+                }
+            }
+            ollama_headers = {"Content-Type": "application/json"}
+            response = requests.post(api_url, headers=ollama_headers, json=data)
             if response.status_code == 200:
                 response_data = response.json()
                 prompt_response = response_data.get('response', 'No response text found')
                 
                 # Ensure there is a response to construct the full description
                 if prompt_response != 'No response text found':
-                    description = f"{embellish_content} {prompt_response} {style_content}".strip()
-                    return image_prompt, description, neg_content
+                    return prompt_response
                 else:
-                    return image_prompt, "No valid response generated for the image.", neg_content
+                    return "No valid response generated for the image."
             else:
                 print(f"Failed to fetch response, status code: {response.status_code}")
-                return image_prompt, "Failed to fetch response from Ollama.", neg_content
-        except Exception as e:
-            print(f"Exception occurred: {e}")
-            return image, "Exception occurred while processing image.", "No neg content"
-        
+                return "Failed to fetch response from Ollama."
 
     @classmethod
     def INPUT_TYPES(cls):
         node = cls()
+        default_engine = "ollama"
         return {
             "required": {
                 "image": ("IMAGE", ),
                 "image_prompt": ("STRING", {"multiline": True, "default": ""}),
-                "select_vision_model": (node.get_vision_models(node.base_ip, node.ollama_port), {}),
+                "engine": (["ollama", "openai", "anthropic"], {"default": default_engine}),
+                "selected_model": (node.get_models(default_engine, node.base_ip, node.ollama_port) + node.get_models("anthropic", node.base_ip, node.ollama_port) + node.get_models("openai", node.base_ip, node.ollama_port), {}),
                 "embellish_prompt": ([name for name, _ in node.embellish_prompts], {}),
                 "style_prompt": ([name for name, _ in node.style_prompts], {}),
                 "neg_prompt": ([name for name, _ in node.neg_prompts], {}),
-                "seed": ("INT", {"default": 0, "min": 0, "max": 4294967295}),
                 "temperature": ("FLOAT", {"default": 0.7, "min": 0.0, "max": 1.0, "step": 0.1}),
-                "top_k": ("INT", {"default": 40, "min": 0, "max": 100}),
-                "repeat_penalty": ("FLOAT", {"default": 1.1, "min": 0.0, "max": 2.0, "step": 0.1}),
-                "num_ctx": ("INT", {"default": 2048, "min": 64, "max": 8192}),
+            },
+            "optional":{
+                "max_tokens": ("INT", {"default": 160, "min": 1, "max": 2048}),
                 "base_ip": ("STRING", {"default": node.base_ip}),
                 "ollama_port": ("STRING", {"default": node.ollama_port}),
             }
@@ -180,6 +277,18 @@ class IFImagePrompt:
     OUTPUT_NODE = True
     CATEGORY = "ImpactFrames💥🎞️"
 
-    
+    @PromptServer.instance.routes.post("/IF_ImagePrompt/get_models")
+    async def get_models_endpoint(request):
+        data = await request.json()
+        engine = data.get("engine")
+        base_ip = data.get("base_ip")
+        ollama_port = data.get("ollama_port")
+        
+        node = IFImagePrompt()
+        models = node.get_models(engine, base_ip, ollama_port)
+        
+        return web.json_response(models)
+
+
 NODE_CLASS_MAPPINGS = {"IF_ImagePrompt": IFImagePrompt}
 NODE_DISPLAY_NAME_MAPPINGS = {"IF_ImagePrompt": "IF Image to Prompt🖼️"}
