bf797be480d3988ea5c9fcf366229e17371af2c1
ImpactFrames <jvoxel@gmail.com>
2024-04-06 14:54:55 +0100

    Update IFPromptMkrNode.py
    fix remote ollama model list

--- IFPromptMkrNode.py
+++ IFPromptMkrNode.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import os
 import textwrap
@@ -5,19 +6,71 @@ import anthropic
 import openai
 from server import PromptServer
 from aiohttp import web
+import tempfile
 
-class IFPrompt2Prompt:
+@PromptServer.instance.routes.post("/IF_PromptMkr/get_models")
+async def get_models_endpoint(request):
+    data = await request.json()
+    engine = data.get("engine")
+    base_ip = data.get("base_ip")
+    port = data.get("port")
+
+    node = IFPrompt2Prompt()
+    models = node.get_models(engine, base_ip, port)
+    return web.json_response(models)
+
+class IFPrompt2Prompt: 
+    RETURN_TYPES = ("STRING", "STRING", "STRING",)
+    RETURN_NAMES = ("Question", "Response", "Negative",)
+    FUNCTION = "sample"
+    OUTPUT_NODE = False
+    CATEGORY = "ImpactFrames💥🎞️"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        node = cls()
+        return {
+            "required": {
+                "input_prompt": ("STRING", {"multiline": True, "default": "Ancient mega-structure, small lone figure in the foreground"}),
+                "base_ip": ("STRING", {"default": node.base_ip}),
+                "port": ("STRING", {"default": node.port}),
+                "engine": (["ollama", "openai", "anthropic"], {"default": node.engine}),
+                #"selected_model": (node.get_models("node.engine", node.base_ip, node.port), {}), 
+                "selected_model": ((), {}),
+                "embellish_prompt": ([name for name, _ in node.embellish_prompts], {}),
+                "style_prompt": ([name for name, _ in node.style_prompts], {}),
+                "neg_prompt": ([name for name, _ in node.neg_prompts], {}),
+                "temperature": ("FLOAT", {"default": 0.7, "min": 0.0, "max": 1.0, "step": 0.1}),
+            },
+            "optional": {
+                "max_tokens": ("INT", {"default": 160, "min": 1, "max": 2048}),
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                "random": ("BOOLEAN", {"default": False, "label_on": "Seed", "label_off": "Temperature"}),
+            },
+            "hidden": {
+                "model": ("STRING", {"default": ""}),
+            },
+        }
+    @classmethod
+    def IS_CHANGED(cls, engine, base_ip, port):
+        node = cls()
+        if engine != node.engine or base_ip != node.base_ip or port != node.port or node.selected_model != node.get_models(engine, base_ip, port):
+            node.engine = engine
+            node.base_ip = base_ip
+            node.port = port
+            node.selected_model = node.get_models(engine, base_ip, port)
+            return True
+        return False
 
     def __init__(self):
+        self.base_ip = "localhost" 
+        self.port = "11434"     
+        self.engine = "ollama" 
+        self.selected_model = ""
         self.script_dir = os.path.dirname(os.path.abspath(__file__))
         self.neg_prompts = self.load_presets(os.path.join(self.script_dir, "negfiles"))
         self.embellish_prompts = self.load_presets(os.path.join(self.script_dir, "embellishfiles"))
-        self.style_prompts = self.load_presets(os.path.join(self.script_dir, "stylefiles"))
-        self.base_ip = "127.0.0.1"
-        self.ollama_port = "11434"
-        self.anthropic_api_key = self.get_api_key("ANTHROPIC_API_KEY")
-        self.openai_api_key = self.get_api_key("OPENAI_API_KEY")
-
+        self.style_prompts = self.load_presets(os.path.join(self.script_dir, "stylefiles")) 
         self.prime_directive = textwrap.dedent("""\
             Act as a prompt maker with the following guidelines:
             - Break keywords by commas.
@@ -40,31 +93,16 @@ class IFPrompt2Prompt:
             Make a prompt for the following Subject:
             """)
 
-    def load_presets(self, dir_path):
-        presets = []
-        for filename in os.listdir(dir_path):
-            if filename.endswith(".txt"):
-                file_path = os.path.join(dir_path, filename)
-                with open(file_path, 'r') as file:
-                    content = file.read().strip()
-                    presets.append((os.path.splitext(filename)[0], content))
-        return presets
 
-    def get_api_key(self, api_key_name):
-        api_key = os.getenv(api_key_name)
-        if api_key:
-            print(f"API key found for {api_key_name}")
-            return api_key
-        print(f"Error: {api_key_name} is required")
-        return ""
-
-    def get_models(self, engine, base_ip, ollama_port):
+    def get_models(self, engine, base_ip, port):
         if engine == "ollama":
-            api_url = f'http://{base_ip}:{ollama_port}/api/tags'
+            api_url = f'http://{base_ip}:{port}/api/tags'
             try:
                 response = requests.get(api_url)
                 response.raise_for_status()
-                return [model['name'] for model in response.json().get('models', [])]
+                models = [model['name'] for model in response.json().get('models', [])]
+                print(f"this are the models {models} from {base_ip}")
+                return models
             except Exception as e:
                 print(f"Failed to fetch models from Ollama: {e}")
                 return []
@@ -76,19 +114,43 @@ class IFPrompt2Prompt:
             print(f"Unsupported engine - {engine}")
             return []
 
-    def sample(self, input_prompt, engine, selected_model, embellish_prompt, style_prompt, neg_prompt, base_ip, ollama_port, temperature, max_tokens):
+    def load_presets(self, dir_path):
+        presets = []
+        for filename in os.listdir(dir_path):
+            if filename.endswith(".txt"):
+                file_path = os.path.join(dir_path, filename)
+                with open(file_path, 'r') as file:
+                    content = file.read().strip()
+                    presets.append((os.path.splitext(filename)[0], content))
+        return presets
+   
+    def get_api_key(self, api_key_name, engine):
+        if engine != "ollama":  
+            api_key = os.getenv(api_key_name)
+            if api_key:
+                return api_key
+        else:
+            print(f'you are using ollama as the engine, no api key is required')
+              
+    def sample(self, input_prompt, engine, base_ip, port, selected_model, embellish_prompt, style_prompt, neg_prompt, temperature, max_tokens, seed, random):
         embellish_content = next((content for name, content in self.embellish_prompts if name == embellish_prompt), "")
         style_content = next((content for name, content in self.style_prompts if name == style_prompt), "")
         neg_content = next((content for name, content in self.neg_prompts if name == neg_prompt), "")
-
-        available_models = self.get_models(engine, base_ip, ollama_port)
-        if available_models is None or selected_model not in available_models:
-            error_message = f"Invalid model selected: {selected_model} for engine {engine}. Available models: {available_models}"
-            print(error_message)
-            raise ValueError(error_message)
-            
+        
         if engine == "anthropic":
-            data = {
+            if random == True:
+                data = {
+                    'model': selected_model,
+                    'system': self.prime_directive, 
+                    'messages': [
+                        {"role": "user", "content": input_prompt}  
+                    ],
+                    'temperature': temperature,
+                    'random': random,
+                    'max_tokens': max_tokens      
+                }
+            else:
+                data = {
                 'model': selected_model,
                 'system': self.prime_directive, 
                 'messages': [
@@ -98,37 +160,61 @@ class IFPrompt2Prompt:
                 'max_tokens': max_tokens      
             }
         elif engine == "openai":
-            data = {
-                'model': selected_model, 
-                'messages': [
-                    {"role": "system", "content": self.prime_directive},
-                    {"role": "user", "content": input_prompt}
-                ],
-                'temperature': temperature,
-                'max_tokens': max_tokens  
-            }
+            if random == True:
+                data = {
+                    'model': selected_model, 
+                    'messages': [
+                        {"role": "system", "content": self.prime_directive},
+                        {"role": "user", "content": input_prompt}
+                    ],
+                    'temperature': temperature,
+                    'seed': seed,
+                    'max_tokens': max_tokens  
+                }
+            else:
+                data = {
+                    'model': selected_model, 
+                    'messages': [
+                        {"role": "system", "content": self.prime_directive},
+                        {"role": "user", "content": input_prompt}
+                    ],
+                    'temperature': temperature,
+                    'max_tokens': max_tokens  
+                }
         else:
-            data = {
-                'model': selected_model, 
-                'messages': [
-                    {"role": "system", "content": self.prime_directive},
-                    {"role": "user", "content": input_prompt}
-                ],
-                'temperature': temperature,
-                'max_tokens': max_tokens
-            }
+            if random == True:
+                data = {
+                    'model': selected_model, 
+                    'messages': [
+                        {"role": "system", "content": self.prime_directive},
+                        {"role": "user", "content": input_prompt}
+                    ],
+                    'temperature': temperature, 
+                    'seed': seed,
+                    'max_tokens': max_tokens
+                }
+            else:
+                data = {
+                    'model': selected_model, 
+                    'messages': [
+                        {"role": "system", "content": self.prime_directive},
+                        {"role": "user", "content": input_prompt}
+                    ],
+                    'temperature': temperature,
+                    'max_tokens': max_tokens
+                }
 
-        generated_text = self.send_request(data, headers={"Content-Type": "application/json"}, engine=engine)
- 
+        generated_text = self.send_request(engine, base_ip, port, data, headers={"Content-Type": "application/json"})
+
         if generated_text:
             combined_prompt = f"{embellish_content} {generated_text} {style_content}"
             return input_prompt, combined_prompt, neg_content
         else:
             return None, None, None
-
-    def send_request(self, data, headers, engine):
+              
+    def send_request(self, engine, base_ip, port, data, headers):
         if engine == "ollama":
-            base_url = f'http://{self.base_ip}:{self.ollama_port}/v1/chat/completions'
+            base_url = f'http://{base_ip}:{port}/v1/chat/completions'
             response = requests.post(base_url, headers=headers, json=data)
             if response.status_code == 200:
                 return response.json().get('choices', [{}])[0].get('message', {}).get('content', '')
@@ -136,10 +222,11 @@ class IFPrompt2Prompt:
                 print(f"Error: Request failed with status code {response.status_code}")
                 return None
         elif engine == "anthropic":
+            anthropic_api_key = self.get_api_key("ANTHROPIC_API_KEY", engine)
             try:
                 base_url = 'https://api.anthropic.com/v1/messages'
                 anthropic_headers = {
-                    "x-api-key": self.anthropic_api_key,
+                    "x-api-key": anthropic_api_key,
                     "anthropic-version": "2023-06-01",  
                     "Content-Type": "application/json"
                 }
@@ -155,10 +242,11 @@ class IFPrompt2Prompt:
                 print(f"Error: Anthropic request failed - {e}")
                 return None
         elif engine == "openai":
+            openai_api_key = self.get_api_key("OPENAI_API_KEY", engine)
             try:
                 base_url = 'https://api.openai.com/v1/chat/completions'
                 openai_headers = {
-                    "Authorization": f"Bearer {self.openai_api_key}",
+                    "Authorization": f"Bearer {openai_api_key}",
                     "Content-Type": "application/json"
                 }
                 response = requests.post(base_url, headers=openai_headers, json=data)
@@ -181,45 +269,9 @@ class IFPrompt2Prompt:
                 print(f"Error: OpenAI request failed - {e}")
                 return None, None, None
 
-    @classmethod
-    def INPUT_TYPES(cls):
-        node = cls()
-        default_engine = "ollama"
-        return {
-            "required": {
-                "input_prompt": ("STRING", {"multiline": True, "default": "Ancient mega-structure, small lone figure in the foreground"}),
-                "engine": (["ollama", "openai", "anthropic"], {"default": default_engine}),
-                "selected_model": (node.get_models(default_engine, node.base_ip, node.ollama_port) + node.get_models("anthropic", node.base_ip, node.ollama_port) + node.get_models("openai", node.base_ip, node.ollama_port), {}),
-                "embellish_prompt": ([name for name, _ in node.embellish_prompts], {}),
-                "style_prompt": ([name for name, _ in node.style_prompts], {}),
-                "neg_prompt": ([name for name, _ in node.neg_prompts], {}),
-                "temperature": ("FLOAT", {"default": 0.7, "min": 0.0, "max": 1.0, "step": 0.1}),
-            },
-            "optional": {
-                "max_tokens": ("INT", {"default": 160, "min": 1, "max": 2048}),
-                "base_ip": ("STRING", {"default": node.base_ip}),
-                "ollama_port": ("STRING", {"default": node.ollama_port}),
-            }
-        }
-
-    RETURN_TYPES = ("STRING", "STRING", "STRING",)
-    RETURN_NAMES = ("Question", "Response", "Negative",)
-    FUNCTION = "sample"
-    OUTPUT_NODE = False
-    CATEGORY = "ImpactFrames💥🎞️"
-
-
-    @PromptServer.instance.routes.post("/IF_PromptMkr/get_models")
-    async def get_models_endpoint(request):
-        data = await request.json()
-        engine = data.get("engine")
-        base_ip = data.get("base_ip")
-        ollama_port = data.get("ollama_port")
-        
-        node = IFPrompt2Prompt()
-        models = node.get_models(engine, base_ip, ollama_port)
-        
-        return web.json_response(models)
 
 NODE_CLASS_MAPPINGS = {"IF_PromptMkr": IFPrompt2Prompt}
 NODE_DISPLAY_NAME_MAPPINGS = {"IF_PromptMkr": "IF Prompt to Prompt💬"}
+
+
+
