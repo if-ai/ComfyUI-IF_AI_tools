fd153841474a97a02a8459694dc88180f9129224
ImpactFrames <jvoxel@gmail.com>
2024-04-06 14:57:04 +0100

    Update IFDisplayTextNode.py
    fix list issue

--- IFDisplayTextNode.py
+++ IFDisplayTextNode.py
@@ -1,27 +1,28 @@
 class IFDisplayText:
     def __init__(self):
-        pass
+        self.type = "output"
 
     @classmethod
-    def INPUT_TYPES(s):
+    def INPUT_TYPES(cls):
 
         return {
             "required": {        
                 "text": ("STRING", {"forceInput": True}),     
                 },
-            "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
+            "hidden": {},
             }
 
     RETURN_TYPES = ("STRING",)
     FUNCTION = "display_text"
     OUTPUT_NODE = True
-    OUTPUT_IS_LIST = (True,)
     CATEGORY = "ImpactFrames💥🎞️"
     
-    def display_text(self, text, prompt=None, extra_pnginfo=None):
+    def display_text(self, text):
+        print("==================")
+        print("IF_AI_tool_output:")
+        print("==================")
+        print(text)
         return {"ui": {"string": [text,]}, "result": (text,)}
     
 NODE_CLASS_MAPPINGS = {"IF_DisplayText": IFDisplayText}
 NODE_DISPLAY_NAME_MAPPINGS = {"IF_DisplayText": "IF Display Text📟"}
-
-
