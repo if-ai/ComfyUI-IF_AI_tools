afacb7b38fa63f6cd12852ca8e14058f221d4287
ImpactFrames <jvoxel@gmail.com>
2024-04-17 03:23:16 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -7,13 +7,21 @@ from .IFImagePromptNode import IFImagePrompt
 from .IFSaveTextNode import IFSaveText
 from .IFDisplayTextNode import IFDisplayText
 from .IFHFDownloadNode import IFHFDownload
+from .IFDreamTalkNode import IFDreamTalk
+from .IFWhisperSpeechNode import IFWhisperSpeech
+from .IFBarkTTSNode import IFBarkTTS
+from .IFChatPromptNode import IFChatPrompt    
 
 NODE_CLASS_MAPPINGS = {
     "IF_PromptMkr": IFPrompt2Prompt,
     "IF_ImagePrompt": IFImagePrompt,
     "IF_SaveText": IFSaveText,
     "IF_DisplayText": IFDisplayText,
-    "IF_HFDownloadNode": IFHFDownload
+    "IF_HFDownloadNode": IFHFDownload,
+    "IF_DreamTalk": IFDreamTalk,
+    "IF_WhisperSpeech": IFWhisperSpeech,
+    "IF_BarkTTS": IFBarkTTS,
+    "IF_ChatPrompt": IFChatPrompt,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -21,7 +29,11 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "IF_ImagePrompt": "IF Image to Prompt🖼️",
     "IF_SaveText": "IF Save Text📝",
     "IF_DisplayText": "IF Display Text📟",
-    "IF_HFDownloadNode": "Hugging Face Download🤗"
+    "IF_HFDownloadNode": "Hugging Face Download🤗",
+    "IF_DreamTalk": "IF DreamTalk🧏🏻",
+    "IF_WhisperSpeech": "IF Whisper Speech🌬️",
+    "IF_BarkTTS": "IF Bark TTS🐶",
+    "IF_ChatPrompt": "IF Chat Prompt👨‍💻"
 }
 
 WEB_DIRECTORY = "./web"
