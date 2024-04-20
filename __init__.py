11532fdaefdbfe343e3fd5cb4505980f8e938da6
ImpactFrames <jvoxel@gmail.com>
2024-04-20 17:50:18 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,6 @@ from .IFPromptMkrNode import IFPrompt2Prompt
 from .IFImagePromptNode import IFImagePrompt
 from .IFSaveTextNode import IFSaveText
 from .IFDisplayTextNode import IFDisplayText
-from .IFHFDownloadNode import IFHFDownload
 from .IFChatPromptNode import IFChatPrompt    
 
 NODE_CLASS_MAPPINGS = {
@@ -14,7 +13,6 @@ NODE_CLASS_MAPPINGS = {
     "IF_ImagePrompt": IFImagePrompt,
     "IF_SaveText": IFSaveText,
     "IF_DisplayText": IFDisplayText,
-    "IF_HFDownloadNode": IFHFDownload,
     "IF_ChatPrompt": IFChatPrompt,
 }
 
@@ -23,7 +21,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "IF_ImagePrompt": "IF Image to Prompt🖼️",
     "IF_SaveText": "IF Save Text📝",
     "IF_DisplayText": "IF Display Text📟",
-    "IF_HFDownloadNode": "Hugging Face Download🤗",
     "IF_ChatPrompt": "IF Chat Prompt👨‍💻"
 }
 
